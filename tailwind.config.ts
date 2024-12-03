b07a2dcbbd60e44d37e627dfd8fa84376740c56f
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-12-03 23:41:10 +0700

    update color and picture

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // ใช้ CSS Variable
+        foreground: "var(--foreground)", // ใช้ CSS Variable
+        primary: "#FF5733",
+        secondary: "#DEDEDE",
+        accent: "", 
+        neutral: "",
       },
     },
   },
